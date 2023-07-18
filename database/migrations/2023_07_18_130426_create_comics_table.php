87c3a1c39a0809b1d3ebf3f527d0d15cb5d67a97
Alecpu92 <alessandro_belfiore@hotmail.it>
2023-07-18 15:19:14 +0200

    importati dati su  migration e cambiato file .env

--- database/migrations/2023_07_18_130426_create_comics_table.php
+++ database/migrations/2023_07_18_130426_create_comics_table.php
@@ -14,8 +14,22 @@ return new class extends Migration
     public function up()
     {
         Schema::create('comics', function (Blueprint $table) {
-            $table->id();
-            $table->timestamps();
+            $table -> id();
+
+
+            $table -> string('title');
+            $table -> text('description') -> nullable();
+            $table -> string('thumb') -> nullable();
+            $table -> string('price');
+            $table -> string('series');
+            $table -> string('sale_date');
+            $table -> string('type');
+
+
+
+
+
+            $table-> timestamps();
         });
     }
 
