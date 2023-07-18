84a38575e00d4ab97e2fbeaa6907c2822bad2772
Alecpu92 <alessandro_belfiore@hotmail.it>
2023-07-18 16:17:02 +0200

    lavorati nuovi components e controller

---
+++ resources/views/Layouts/home.blade.php
@@ -0,0 +1,7 @@
+@extends ("layouts.main-layout")
+
+@section("content")
+
+<h1>Hello, World from home.blade.php</h1>
+
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -1,23 +1,14 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+<?php
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
+use App\Http\Controllers\MainController;
 
-  
+Route :: get('/', [MainController :: class, 'index'])
+   -> name("index");
 
-    return view('welcome');
-});
 
 
