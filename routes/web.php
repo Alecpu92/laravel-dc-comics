903469dd80fc4f6d84eed0b5fe084898557ae881
Alecpu92 <alessandro_belfiore@hotmail.it>
2023-07-19 16:27:22 +0200

    aggiornate rotte su web.php

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,13 @@ Route :: get('/create', [MainController :: class, 'create'])
 Route :: post('/store', [MainController :: class, 'store'])
     -> name('store');
 
+Route :: get('/edit/{id}', [MainController :: class, 'edit'])
+    -> name('edit');
+Route :: put('/update/{id}', [MainController :: class, 'update'])
+    -> name('update');
+
+Route :: delete('/delete/{id}', [MainController :: class, 'delete'])
+    -> name('delete');
+
 
 
